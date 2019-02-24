c52f908e262666a9a62dfe762314e52bbd241049
devzip8 <pi3kp85@gmail.com>
2019-02-24 19:05:00 +0900

    answer B055

---
+++ rankB/B055_taxi_fee.rb
@@ -0,0 +1,18 @@
+n, x = gets.chomp.split(' ').map(&:to_i)
+
+min = -1
+max = 0
+n.times do |i|
+  ax, bf, cx, df = gets.chomp.split(' ').map(&:to_i)
+  fee = bf # 初乗り
+  if x >= ax
+    extra_x = x - ax
+    extra = (extra_x / cx) + 1
+    fee += extra * df
+  end
+
+  min = fee if fee < min || min < 0 
+  max = fee if fee > max
+end
+
+puts "#{min} #{max}"
