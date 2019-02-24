3593194e2094a130f49e9442208bb82b5282b438
devzip8 <pi3kp85@gmail.com>
2019-02-24 20:08:14 +0900

    fix B053

--- rankB/B053_auto_table.rb
+++ rankB/B053_auto_table.rb
@@ -1,34 +1,33 @@
 h, w = gets.chomp.split(' ').map(&:to_i)
-a11, a12 = gets.chomp.split(' ').map(&:to_i)
-b21, b22 = gets.chomp.split(' ').map(&:to_i)
-
-diff_h = a12 - a11
-diff_v = b21 - a11
+a00, a01 = gets.chomp.split(' ').map(&:to_i)
+a10, a11 = gets.chomp.split(' ').map(&:to_i)
 
 arr = []
 arr << Array.new(w, 0)
 arr << Array.new(w, 0)
 
-arr[0][0] = a11
-arr[0][1] = a12
-arr[1][0] = b21
-arr[1][1] = b22
+arr[0][0] = a00
+arr[0][1] = a01
+arr[1][0] = a10
+arr[1][1] = a11
 
+# まずは1, 2行目を全て埋める
 (2...w).each do |wi|
-  arr[0][wi] = arr[0][1] + (a12 - a11) * (wi -1)
-  arr[1][wi] = arr[1][1] + (b22 - b21) * (wi -1)
+  arr[0][wi] = arr[0][1] + (a01 - a00) * (wi - 1)
+  arr[1][wi] = arr[1][1] + (a11 - a10) * (wi - 1)
 end
 
-w.times do |wi|
-  diff_v = arr[1][wi] - arr[0][wi]
-  (2...h).each do |hi|
-    # puts hi
-    
-    arr << Array.new(w) if wi == 0 && hi >= 2
-    # p arr
-    ret = arr[1][wi] + diff_v * (hi - 1)
-    # puts "#{hi}, #{wi}"
-    arr[hi][wi] = ret
+# 次に1, 2列目を全て埋める
+(2...h).each do |hi|
+  arr << Array.new(w)
+  arr[hi][0] = arr[1][0] + (a10 - a00) * (hi - 1)
+  arr[hi][1] = arr[1][1] + (a11 - a01) * (hi - 1)
+end
+
+# 3列目以降、３行目以降を埋めていく
+(2...h).each do |hi|
+  (2...w).each do |wi|
+    arr[hi][wi] = arr[hi][1] + (arr[hi][1] - arr[hi][0]) * (wi - 1)
   end
 end
 
