460353fabdf1d9cb8e33c3fe29df0d429f325d5e
devzip8 <pi3kp85@gmail.com>
2019-02-24 18:40:04 +0900

    answer B027

---
+++ rankB/B027_cardgame.rb
@@ -0,0 +1,29 @@
+h, w, n = gets.chomp.split(' ').map(&:to_i)
+cards = []
+h.times do |i|
+  row = gets.chomp.split(' ').map(&:to_i)
+  cards << row
+end
+
+#p cards
+#p  cards[1-1][1-1]
+#p  cards[2-1][1-1]
+
+players = Array.new(n) { |idx| 0 }
+pi = 0
+
+l = gets.chomp.to_i
+l.times do |j|
+  a1, b1, a2, b2 = gets.chomp.split(' ').map(&:to_i)
+  if cards[a1-1][b1-1] == cards[a2-1][b2-1]
+    players[pi] += 2
+     # めくれた場合はプレイヤーは変わらない
+    next
+  end
+
+  # 次のプレイヤーへ
+  pi += 1
+  pi = 0 if pi >= n
+end
+
+players.each { |p| puts p }
